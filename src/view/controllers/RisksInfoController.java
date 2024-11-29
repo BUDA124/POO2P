b2a608f1d671efc527de805c3833eebbba3a9c66
BUDA124 <castillocarlosccm.421@gmail.com>
2024-11-29 09:03:51 -0600

    Titulo proyecto

--- src/view/controllers/RisksInfoController.java
+++ src/view/controllers/RisksInfoController.java
@@ -2,10 +2,62 @@ package view.controllers;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.control.TextArea;
+
 public class RisksInfoController {
+    @FXML
+    private TextArea textArea;
+    private int currentIndex = 0;
 
+    private final String[] texts = {"Riesgos Biológicos: Exposición a bacterias, virus, insectos, reptiles y plantas que tienen potencial de peligro.\n" +
+            "Prevenciones:\n" +
+            "a) Control de Plagas.\n" +
+            "b) Higiene en la zona y trabajadores.\n" +
+            "c) Protección Física.",
+    "Caídas: Las caídas representan el 40 % de las muertes entre los trabajadores de la construcción, siendo el mayor peligro.\n" +
+            "Prevenciones:\n" +
+            "a) Protección contra caídas como arneses y cascos al trabajar en alturas.\n" +
+            "b) Inspección de los equipos de protección contra caídas.\n" +
+            "c) Medidas de prevención en superficies de trabajo seguras.\n" +
+            "d) Inspección constante de barandas, conectores y tablones para detectar daños y herramientas también.\n",
+    "Lesiones por Golpes: Las lesiones por golpes pueden ser causadas por objetos y malas prácticas.\n" +
+            "Prevenciones:\n" +
+            "a) Uso de protección.\n" +
+            "b) Mantenimiento adecuado de maquinaria.\n" +
+            "c) Implementación de sistemas de señalización.\n" +
+            "d) Uso de ropa de alta visibilidad y EPP adecuado.\n" +
+            "e) Implementación de barreras de protección y planes de control de tráfico en la obra. \n",
+    "Peligros de Electricidad: Los peligros eléctricos incluyen el contacto con cables, objetos electrificados y el mal manejo de conexiones.\n" +
+            "Prevenciones:\n" +
+            "a) Mantener distancias y usar equipos aislantes.\n" +
+            "b) Asegurar instalaciones eléctricas.\n" +
+            "c) Evitar condiciones peligrosas.\n",
+    ". Riesgos Físicos: Incluyen exposición a ruidos, temperaturas y vibraciones.\n" +
+            "Prevenciones:\n" +
+            "a) Uso de protectores auditivos.\n" +
+            "b) Control de Temperaturas.\n" +
+            "c) Uso de herramientas y maquinaria con sistemas de amortiguación.\n",
+    " Riesgos Ergonómicos:\u2028Relacionados con la manipulación manual de cargas, posturas forzadas y tareas repetitivas.\n" +
+            "Prevenciones:\n" +
+            "a) Ayudas mecánicas como carretillas y montacargas.\n" +
+            "b) Uso de herramientas que reduzcan el esfuerzo físico y minimicen el riesgo de lesiones.\n",
+    "Riesgos Químicos:\u2028Exposición a productos químicos como disolventes, pinturas, cemento, etcétera.\n" +
+            "Prevenciones:\n" +
+            "a) Uso de guantes, mascarillas y protección en los ojos al manipular sustancias químicas.\n" +
+            "b) Almacenamiento de productos químicos en lugares seguros.\n" +
+            "c) Implementación de sistemas de ventilación en áreas donde se manipulan productos químicos.\n"};
 
+    @FXML
+    private void handleNextButton() {
+        System.out.println("El botón 'Next' fue presionado.");
 
+        if (textArea == null) {
+            System.err.println("El TextArea no está inicializado. Revisa el fx:id o el controlador.");
+            return;
+        }
+        currentIndex = (currentIndex + 1) % texts.length;
+        textArea.setText(texts[currentIndex]);
+    }
 
     @FXML
     private void returnImageHandler() {
--- src/view/controllers/ToolsInfoController.java
+++ src/view/controllers/ToolsInfoController.java
@@ -8,39 +8,32 @@ import javafx.scene.image.ImageView;
 public class ToolsInfoController {
 
     @FXML
-    private ImageView toolImage;
-
-    @FXML
-    private Button nextButton;
-
-    private Image[] images;
+    private TextArea textArea;
     private int currentIndex = 0;
 
+    private final String[] texts = {"Generador Eléctrico: Fuente de energía esencial para operar maquinaria y herramientas en lugares sin acceso a red eléctrica. Riesgo: Riesgo de electrocución si no se maneja correctamente o si hay exposición a cables dañados y de intoxicación por gases (CO, CO2) en espacios cerrados debido a la combustión del generador. Prevención: Realizar inspecciones periódicas del cableado y conexiones, asegurarse de que los cables estén aislados, en buen estado y en áreas bien ventiladas.",
+    "Hormigonera: Máquina utilizada para mezclar y preparar hormigón. Riesgo: Exposición a polvo y partículas, lo cual puede causar problemas respiratorios. Prevención: Usar mascarillas y gafas de protección para evitar inhalar polvo y proteger los ojos.",
+    "Placa Compactadora: Herramienta utilizada para compactar suelos y garantizar una base sólida para las estructuras. Riesgo: Ruido excesivo, lo cual puede afectar la audición si no se usan protectores adecuados. Prevención: Usar protectores auditivos adecuados para prevenir daño auditivo.\n",
+            "Carretilla Elevadora: Equipamiento utilizado para transportar materiales pesados. Riesgo: Riesgo de atropello, tanto para el operario como para otros trabajadores en la obra. Prevención: Señalizar la zona de trabajo y limitar el acceso de personas no autorizadas cuando la carretilla está en uso.",
+    "Nivel y Destornillador: Herramientas para asegurar la alineación y ensamblaje de las estructuras. Riesgo: Riesgo de corte o pinchazo si el destornillador se usa incorrectamente. Prevención: Usar destornilladores adecuados para cada tipo de tornillo y aplicar fuerza moderada para evitar resbalones.",
+    "Llaves, Pinzas y Remachadora: Utilizadas para ensamblar componentes y asegurar las estructuras metálicas. Riesgo: Riesgo de atrapamiento o aplastamiento de dedos al ensamblar piezas y de proyección de piezas o fragmentos. Prevención: Usar gafas de seguridad al remachar para protegerse de posibles proyecciones y mantener las manos y dedos alejados de las piezas en movimiento\n",
+    "Serrucho, Pala y Martillo: Herramientas manuales para cortar, cavar y golpear materiales. Riesgo: Riesgo de cortes o heridas en las manos por el uso del serrucho, lesiones en la espalda al usar la pala incorrectamente y de golpes en las manos o dedos al manejar el martillo. Prevención: Usar guantes de protección y técnicas adecuadas de levantamiento",
+    "Taladro y Amoladora: Equipos eléctricos utilizados para perforar y cortar materiales. Riesgo: Riesgo de proyección de fragmentos y electrocución si no hay aislamiento adecuado. Prevención: Usar gafas y asegurarse de usar el taladro y la amoladora en superficies secas"};
+
     @FXML
-    public void initialize() {
-        images = new Image[]{
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools1.PNG")),
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools2.PNG")),
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools3.PNG")),
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools4.PNG")),
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools5.PNG")),
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools6.PNG")),
-                new Image(getClass().getResourceAsStream("view/scenes/images/tools7.PNG"))
-        };
+    private void handleNextButton() {
+        System.out.println("El botón 'Next' fue presionado.");
 
-        toolImage.setImage(images[currentIndex]);
-
-        nextButton.setOnAction(event -> rotateImage());
-    }
-
-    public void rotateImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        toolImage.setImage(images[currentIndex]);
+        if (textArea == null) {
+            System.err.println("El TextArea no está inicializado. Revisa el fx:id o el controlador.");
+            return;
+        }
+        currentIndex = (currentIndex + 1) % texts.length;
+        textArea.setText(texts[currentIndex]);
     }
 
     @FXML
     private void returnImageHandler() {
         SceneController.changeScene("/view/scenes/informationMenuWindow.fxml");
     }
-
 }
